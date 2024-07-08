3c55fa6be07f3dce88e33c8feb2c0ac67f5eaffa
sulea <3168162174@qq.com>
2024-07-08 18:04:16 +0800

    add

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,9 @@ const {verifyAndRefreshTokens} = require('../utils/tokenManager');
 // 引入路由文件
 const indexRouter = require('./index');
 const usersRouter = require('./users');
-const UploadRouter = require('./files');
 const projectsRouter = require('./projects');
 // 设置 /users 路由前缀
 router.use('/', indexRouter);
 router.use('/users', usersRouter);
-router.use("/files", verifyAndRefreshTokens, UploadRouter);
 router.use('/projects', projectsRouter);
 module.exports = router;
